dd07d258d3cb8876de992cb480c2a06498ba2345
Wayne Piekarski <wayne@tinmith.net>
2018-01-22 10:27:36 -0800

    Add license files for GPLv3 and copyright notices

---
+++ update-license.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+for each in `find . -name "*.java" -o -name "*.c" -o -name "*.h" -o -name "*.kt"`; do
+  # Need to determine if any license text exists before we try to change it
+  SEPARATOR=" ---------------------------------------------------------------------"
+  EXISTS=`grep -- "$SEPARATOR" $each`
+  if [ "$EXISTS" = "" ]; then
+    # No license exists, just add it to the top, along with an extra blank line
+    (cat COPYRIGHT; echo; cat $each) > $each.temp
+  else
+    # License text exists, use sed to remove it
+    (cat COPYRIGHT; cat $each | sed "1,/$SEPARATOR/d") > $each.temp
+  fi
+  mv $each.temp $each
+done
