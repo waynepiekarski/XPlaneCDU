8014e91d5670278ba23ee63170dbd094c51d154c
Wayne Piekarski <wayne@tinmith.net>
2018-02-02 21:05:45 -0800

    Copy DatagramPacket address to prevent delays or deadlock that can happen if done in the UI thread

--- app/src/main/java/net/waynepiekarski/xplanecdu/MulticastReceiver.kt
+++ app/src/main/java/net/waynepiekarski/xplanecdu/MulticastReceiver.kt
@@ -67,10 +67,11 @@ class MulticastReceiver (address: String, port: Int, internal var callback: OnRe
                             // Log.d(Const.TAG, "Hex dump = [" + bytesToHex(packet.getData(), packet.getLength()) + "]");
                             // Log.d(Const.TAG, "Txt dump = [" + UDPReceiver.bytesToChars(packet.getData(), packet.getLength()) + "]");
                             // getHostAddress appears to block and should not be called on the UI thread!
-                            val data = Arrays.copyOfRange(buffer, 0, packet.length)
                             if (lastAddress == null || !lastAddress!!.equals(packet.address)) {
                                 lastAddress = packet.address
-                                Handler(Looper.getMainLooper()).post { callback.onReceiveMulticast(data, packet.address) }
+                                val copyAddress = packet.address // This uses a mutex and cannot be done within a UI handler
+                                val copyData = Arrays.copyOfRange(buffer, 0, packet.length)
+                                Handler(Looper.getMainLooper()).post { callback.onReceiveMulticast(copyData, copyAddress) }
                             }
                         } catch (e: SocketTimeoutException) {
                             Log.d(Const.TAG, "Timeout, reading again ...");
